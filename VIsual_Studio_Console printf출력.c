970b9bef3c1abd95cef9531a1387aeb0605d3b5c
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-04-11 03:19:31 +0900

    Add files via upload

---
+++ VIsual_Studio_Console printf출력.c
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+
+int _tmain(int argc, _TCHAR * argv[])
+{
+    printf("서식이 있는 것을 출력합니다. \n"); //\n은 한칸 건너뛰기
+    printf("%d\n", 1000);
+
+    printf("%d %d\n", 1, 2, 3);
+    printf("%s\n", "문자열을 처리합니다"); //%s는 문자열 출력
+
+    return 0;
+}
