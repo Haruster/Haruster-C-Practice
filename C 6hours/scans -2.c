1a17bd1c237f724f4c40d85f99cea42a92827f82
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-04-16 04:29:43 +0900

    Create scans -2.c

---
+++ C 6hours/scans -2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+    int a, b, c, d;
+
+    printf("데이터 값을 입력해주세요\n");
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    printf("%d %d %d %d입니다\n", a, b, c, d);
+    return 0;
+}
