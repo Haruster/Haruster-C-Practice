954a43ebfb724fa565c28f2eaba7d63796436db4
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-04-16 04:38:37 +0900

    Create 진수 출력.c

---
+++ src/진수 출력.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main(void) {
+    int x = 10;
+    printf("8진수 출력값은 %o입니다.\n", x);
+    printf("10진수 출력값은 %d입니다.\n", x);
+    printf("16진수 출력값은 %x입니다.\n", x);
+
+    return 0;
+
+}
