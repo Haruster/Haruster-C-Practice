553c973baec75122e15112d485233bae0b7d6e12
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-05-10 16:31:58 +0900

    Update 에라토스 테네스의 체.c

--- 에라토스 테네스의 체.c
+++ 에라토스 테네스의 체.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 #define SIZE 12345
 
-int main(void) {
-    int number[SIZE];
-    int i, j;
-    
-    number[0] = 0;
-    number[1] = 0;
-    
-    for ( i = 2; i < SIZE; i++)
-        number[i] = 1;
-    
-    for (i = 2; i <= 12345; i++) {
-        if (number[i] == 1) {
-            for (j = i + i; j < 12345; j+=1) {
-                number[j] = 0;
-            }
-        }
-        else continue;
-    }
+void main()
+{
+	int number[SIZE];
+	for (int i = 2; i < SIZE; i++)
+		number[i] = i;			
+
+	for (int i = 2; i * 2 < SIZE; i++)
+	{
+		if (number[i] == 0)		
+			continue;
+
+		for (int k = i, j = i; k * j < SIZE; j++)
+			number[k * j] = 0;	
+	}
+
+	for (int i = 0; i < SIZE; ++i)		
+		if (number[i] != 0)
+			printf("%d ", number[i]);
+	
 }
