f51ad18169d0237f3fd193810bf86d75625cac14
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-05-26 21:44:06 +0900

    Create 문자열 이름 출력.c

---
+++ 문자열 이름 출력.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	char name[10] = "NewBongMan";
+
+	for (int i = 0; i < 10; i++) {
+
+		printf("%c", name[i]);
+
+	}
+	printf("\n2021E7334 정준영");
+	return 0;
+
+}
