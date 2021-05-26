3313b0b2a4579810bf49fff7aa94d80551fad4b1
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-05-26 22:57:29 +0900

    Update 문자열 이름 출력.c

--- 문자열 이름 출력.c
+++ 문자열 이름 출력.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 int main(void) {
-	char name[10] = "NewBongMan";
+	char name[10] = "Kinesys";
 
 	for (int i = 0; i < 10; i++) {
 
 		printf("%c", name[i]);
 
 	}
-	printf("\n2021E7334 정준영");
 	return 0;
 
 }
